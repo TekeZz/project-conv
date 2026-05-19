2b94976936464777b4c55bee8bc1ca523a2c5078
TekeZz <jota.amaral585@gmail.com>
2026-05-19 05:21:40 -0300

    input-value

---
+++ project-01/scripts.js
@@ -0,0 +1,10 @@
+const setaButton = document.querySelector (".seta-button")
+
+function convertValues (){
+   const inputCurrencyValue = document.querySelector(".value-input").value
+
+   console.log(inputCurrencyValue)
+}
+
+setaButton.addEventListener ("click", convertValues )
+
