77cbcaaca15ab28d27fba61d0a5efa44327f9f69
TekeZz <jota.amaral585@gmail.com>
2026-05-25 03:37:02 -0300

    changes

--- project-01/scripts.js
+++ project-01/scripts.js
@@ -1,10 +1,32 @@
-const setaButton = document.querySelector (".seta-button")
+const setaButton = document.querySelector(".seta-button")
 
-function convertValues (){
-   const inputCurrencyValue = document.querySelector(".value-input").value
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".value-input").value
+    const currencyValueToCovert = document.getElementById ("toConvert")
+    const currencyValueConverted = document.getElementById ("converted")
+   
 
-   console.log(inputCurrencyValue)
+    const dolarToDay = 5.06
+
+    const convertedValue = inputCurrencyValue / dolarToDay
+
+    currencyValueToCovert.innerHTML = new Intl.NumberFormat ("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat ("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(convertedValue)
+    
 }
 
-setaButton.addEventListener ("click", convertValues )
+setaButton.addEventListener("click", convertValues)
+
+
+    
+
+
+
 
